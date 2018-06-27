794c49a3bd7bb2f05ab9edd612a2f81be39bd93e
FNBJNB01\Mthulisi Nxumalo <Mthulisi.Nxumalo@fnb.co.za>
2018-06-27 18:54:10 +0200

    Autowiring beans

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -2,10 +2,12 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 5/24/17.
  */
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
